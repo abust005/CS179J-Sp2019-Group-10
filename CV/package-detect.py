4ea5e9223a5be6c76b7ef0c680997e5298248542
Adriel Bustamante <abust005@ucr.edu>
2019-04-29 14:47:41 -0700

    Began extracting connected components and BBoxes

--- CV/package-detect.py
+++ CV/package-detect.py
@@ -14,21 +14,22 @@ ret, frame = cap.read()
 
 print(ret)
 
-#while(True):
+while(True):
     # Capture frame-by-frame
-#    ret, frame = cap.read()
+    ret, frame = cap.read()
 
     # Our operations on the frame come here
-#    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    (thresh, im_bw) = cv.threshold(gray, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+    ret, labels, stats = cv.connectedComponentsWithStats(im_bw)
+    boxes = cv.boundingRect()
 
 #    # Display the resulting frame
-#    cv.imshow('frame',frame)
-#    if cv.waitKey(1) & 0xFF == ord('q'):
-#        break
+    cv.imshow('frame',im_bw)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # When everything done, release the capture
-
-
 
 cap.release()
 cv.destroyAllWindows()
