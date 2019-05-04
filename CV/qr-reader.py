a2d98c121fe7d42ac60f94430b362aebdf02f8cf
abust005 <abust005@ucr.edu>
2019-05-04 14:53:41 -0700

    Updated QR reader - avg. time to read .010s

--- CV/qr-reader.py
+++ CV/qr-reader.py
@@ -3,10 +3,7 @@ import numpy as np
 import sys
 import time
 
-
-inputImage = cv.imread("Package1.jpg")
-
-    # Display barcode and QR code location
+# Display barcode and QR code location
 def display(im, bbox):
     n = len(bbox)
     for j in range(n):
@@ -15,9 +12,13 @@ def display(im, bbox):
     # Display results
     cv.imshow("Results", im)
 
+inputImage = cv.imread('C:\\Users\\Adriel\\CS179J-Sp2019-Group-10\\CV\\Package3.jpg')
+
 qrDecoder = cv.QRCodeDetector()
 # Detect and decode the qrcode
+t = time.time()
 data,bbox,rectifiedImage = qrDecoder.detectAndDecode(inputImage)
+print("Time Taken for Detect and Decode : {:.3f} seconds".format(time.time() - t))
 if len(data)>0:
     print("Decoded Data : {}".format(data))
     display(inputImage, bbox)
