214cbf275363b6f456dc9445fd68c03a41531b1a
Jonathan Woolf <jwool003@ucr.edu>
2019-05-29 11:52:04 -0700

    Improve utility of gps.py by wrapping functionality into a convenient function and updating documentation

--- gps/gps.py
+++ gps/gps.py
@@ -3,36 +3,36 @@ import serial
 '''
 README:
 This code reads data from the linux serial port "ttyACM0" and
-writes "latitude, longitude" to the file position.txt in real time
+calls the function "position()" which reads the serial port to both return
+latitude and longitude and output them to the position.txt file
+
+For convenience, the function is called in an infinite while loop and the
+return statement is printed to terminal when the function doesn't return "None"
 
 Use the following command to gain access to the port because life is hard:
 
 sudo chmod 666 /dev/ttyACM0
+
+if you need to find the correct port use:
+
+python -m serial.tools.list_ports
 '''
+
+def position(GPS):
+    line = GPS.readline()
+    data = line.decode().split(",")
+    if data[0] == "$GPRMC":
+        #A means that the GPS is updating properly and returning a real value
+        if data[2] == "A":
+            #write latitude, Longitude to position.txt file
+            with open("position.txt", "w") as pos:
+                pos.write(data[3] + ", " + data[5] + "\n")
+            return(data[3], data[5])
 
 GPS = serial.Serial("/dev/ttyACM0", baudrate = 9600)
 if(GPS.is_open): print(GPS.name, "is open!")
-#else: print(GPS.name, "is currently protecting its chastity!")
 
-i = 0
-count = 1
 while True:
-        i = i + 1;
-        line = GPS.readline()
-        data = line.decode().split(",")
-        if data[0] == "$GPRMC":
-            if data[2] == "A":
-                #write latitude, Longitude to position.txt file
-                with open("position.txt", "w") as pos:
-                    pos.write(data[3] + ", " + data[5] + "\n")
-
-                #comment this paragraph back in to display 10 or 11 coordinates in the terminal
-                '''
-                print(count)
-                count = count + 1;
-                print("Latitude: $s", data[3])
-                print("Longitude: $s", data[5], "\n")
-
-        if i == 100:
-            exit()
-        '''
+    pos = position(GPS)
+    if pos is not None:
+        print(pos)
